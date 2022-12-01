7f4b0364482d294faaf487e6dfbdf4e76d5498d6
David Proto <davidproto@sbcglobal.net>
2022-11-30 19:24:43 -0500

    pushing to Heroku

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ WHEN I click on an existing note in the list in the left-hand column
 THEN that note appears in the right-hand column
 WHEN I click on the Write icon in the navigation at the top of the page
 THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column.
+
+## Usage
+
+User can view existing notes, or create a new one by clicking on the plus sign and save it by clicking on the save icon.  Notes can be deleted by clicking the red trash can next to the note you wish to remove.
+## Screenshot 
+
+![Alt text](HOMEWORK11.png)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,44 +1,39 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const db = require("../db/db.json");
+let db = require("../db/db.json");
 
 module.exports = function (app) {
-
-  //API GET Requests 
+  //API GET Requests
   app.get("/api/notes", function (req, res) {
     res.json(db);
   });
-  
-  
 
   //API POST Requests
   app.post("/api/notes", function (req, res) {
     let newNote = req.body;
+    let id = db.length.toString();
+    newNote.id = id;
     db.push(newNote);
-    fs.writeFile(
-      path.join(__dirname, "../db/db.json"),
-      JSON.stringify(db),
-      function (err) {
-        if (err) throw err;
-        console.log("Saved my note!");
-        res.json(db);
-      }
-    );
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), function (err) {
+      if (err) throw err;
+    });
+    res.json(db);
   });
 
   //API DELETE Requests
   app.delete("/api/notes/:id", function (req, res) {
-    fs.readFile(
-      path.join(__dirname, "../db/db.json"),
-      "utf8",
-      function (err, data) {
-        if (err) throw err;
-
-        let id = req.params.id;
-        db.splice(id, 1);
-        res.json(db);
-      }
-    );
+    let noteId = req.params.id;
+    let newId = 0;
+    console.log(`Deleting note with id ${noteId}`);
+    db = db.filter((currentNote) => {
+      return currentNote.id != noteId;
+    });
+    for (currentNote of db) {
+      currentNote.id = newId.toString();
+      newId++;
+    }
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    res.json(db);
   });
 };
